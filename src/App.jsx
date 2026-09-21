91a692095d834f1767913a035a0111ac01c8c396
2456454 <62181264+2456454@users.noreply.github.com>
2026-09-21 21:55:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-Import React, { useState } from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
 import Today from "./pages/Today";
 import Jobs from "./pages/Jobs";
